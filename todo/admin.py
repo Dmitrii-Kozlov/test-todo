5ba29c021eacbc81f07ddf239c0796999a09e413
Dimitrii-Kozlov <demetra_n@front.ru>
2022-09-19 18:50:03 +0300

    create todo model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
+from typing import Sequence
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields: Sequence[str] = ('created',)
+
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True, null=True)
+    important = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title 
